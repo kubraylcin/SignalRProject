d7ba3b1f3fe6b1cb8d26971ab91d55158f80dc7c
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-02-09 18:24:23 +0300

    Migration Yüklendi ve İnterfaceler Yazıldı

---
+++ SignalR.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Abstract
+{
+	public interface IGenericDal<T> where T : class // class olmasının nedeni dışarıdan bir nesne methot gibi durumlar girmiş olamayacak
+	{
+		//Tüm interfaceler imzalarımızı tutacak
+		void Add(T entity);
+		void Delete(T entity);
+		void Update(T entity);
+		T GetById(int id);
+		List<T> GetListAll();
+
+	}
+}
