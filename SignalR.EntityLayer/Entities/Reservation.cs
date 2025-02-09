70af7209374decac796c6940fd5101c1d65ccea4
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-02-09 17:47:53 +0300

    N-Tier  ile Entity Katmanı Oluşturuldu

---
+++ SignalR.EntityLayer/Entities/Reservation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.EntityLayer.Entities
+{
+	public class Reservation
+	{
+		public int ReservationId { get; set; }
+		public string Name { get; set;}
+		public string Phone { get; set;}
+		public string Email { get; set;}
+		public int PersonCount { get; set;}
+        public DateTime Date { get; set; }
+
+
+    }
+}
