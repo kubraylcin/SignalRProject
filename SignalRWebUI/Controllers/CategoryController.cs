00bac17541cbcd6e94342986c4c383bfa1b1448d
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-02-15 21:38:48 +0300

    Kategori Listesi API'den Alındı ve Görüntülendi

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.CategoryDtos;
 
 namespace SignalRWebUI.Controllers
 {
 	public class CategoryController : Controller
 	{
-		public IActionResult Index()
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public CategoryController(IHttpClientFactory httpClientFactory)
 		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client= _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7087/api/Category");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData= await responseMessage.Content.ReadAsStringAsync();
+				var values= JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);//JSON verisini bir C# nesnesine çevirmek için resultCategoryDto sınıfını oluşturduk
+				return View(values);//deserialize = json bir datayı çözerken kullanılan yöntem
+			}
 			return View();
 		}
 	}
