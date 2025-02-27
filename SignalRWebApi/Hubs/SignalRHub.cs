5db80682a0409b8656a407564776cda16711d755
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-02-27 12:10:07 +0300

    Sayfa yenilenmeden kategori sayısı getirme işlemi yapıldı

--- SignalRWebApi/Hubs/SignalRHub.cs
+++ SignalRWebApi/Hubs/SignalRHub.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.DataAccessLayer.Concrete;
 
 namespace SignalRWebApi.Hubs
 {
     //hub sunucu görevi görecek dağıtım işlemi hubs sınıfı hnagisiyse onun üzeirnden yapıcaz
     public class SignalRHub:Hub
     {
+        SignalRContext context = new SignalRContext();
+       
+        public async Task SendCategoryCount() // client tarafında gelince bu methot  ınvoke ile  çağrılacak 
+        {
+            var value = context.Categories.Count();
+            await Clients.All.SendAsync("ReceiveCategoryCount", value); //bu method içindeki ReceiveCategoryCount kullan 
+        }
     }
 }
