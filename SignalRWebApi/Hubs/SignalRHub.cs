8b94eaa6647bc18613867fd8c5e7f5b10c0d5d63
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-03-05 11:19:00 +0300

    SignalR ile istatistik sayfamızda anlık verileri görüntüleme işlemi. Kategori Sayısı görüntüleme ürün sayısı görüntüleme  işlemleri

--- SignalRWebApi/Hubs/SignalRHub.cs
+++ SignalRWebApi/Hubs/SignalRHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.BusinessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 
 namespace SignalRWebApi.Hubs
@@ -6,12 +7,33 @@ namespace SignalRWebApi.Hubs
     //hub sunucu görevi görecek dağıtım işlemi hubs sınıfı hnagisiyse onun üzeirnden yapıcaz
     public class SignalRHub:Hub
     {
-        SignalRContext context = new SignalRContext();
-       
-        public async Task SendCategoryCount() // client tarafında gelince bu methot  ınvoke ile  çağrılacak 
+
+		private readonly ICategoryService _categoryService;
+		private readonly IProductService _productService;
+
+		public SignalRHub(ICategoryService categoryService, IProductService productService)
+		{
+			_categoryService = categoryService;
+			_productService = productService;
+		}
+
+		public async Task SendCategoryCount() // client tarafında gelince bu methot  ınvoke ile  çağrılacak 
         {
-            var value = context.Categories.Count();
+            var value = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount", value); //bu method içindeki ReceiveCategoryCount kullan 
         }
+
+		public async Task SendProductCount()
+		{
+			var value2= _productService.TProductCount();
+			await Clients.All.SendAsync("ReceiveProductCount", value2);
+		}
+		public async Task  ActivePassiveCategoryCount()
+		{
+			var value3= _categoryService.TActiveCategoryCount();
+			var value4= _categoryService.TPassiveCategoryCount();
+			await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
+			await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
+		}
     }
 }
