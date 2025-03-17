15c5a1f1f11420e6593af39fa9929e7aeec05353
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-03-17 15:37:07 +0300

    Ana Sayfa Apı Consumed işlemleri yapıldı

--- SignalR.DtoLayer/ContactDto/CreateContactDto.cs
+++ SignalR.DtoLayer/ContactDto/CreateContactDto.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace SignalR.DtoLayer.ContactDto
 {
-	public class CreateContactDto
-	{
-		public string Location { get; set; }
-		public string Phone { get; set; }
-		public string Mail { get; set; }
-		public string FooterDescription { get; set; }
-	}
+    public class CreateContactDto
+    {
+        public string Location { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        public string FooterTitle { get; set; }
+        public string FooterDescription { get; set; }
+        public string OpenDays { get; set; }
+        public string OpenDaysDescription { get; set; }
+        public string OpenHourse { get; set; }
+    }
 }
