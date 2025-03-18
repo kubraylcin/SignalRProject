d942363abf62dd58fa46d255cda6dedf82ef7d11
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-03-18 17:43:30 +0300

    Ürün Adı İçeren Sepet Listesi API Metodu Eklendi

--- SignalRWebApi/Controllers/BasketController.cs
+++ SignalRWebApi/Controllers/BasketController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalRWebApi.Models;
 
 namespace SignalRWebApi.Controllers
 {
@@ -18,6 +21,22 @@ namespace SignalRWebApi.Controllers
         public IActionResult GetBasketsByTableNumber(int id) 
         { 
             var  values = _basketService.TGetBasketsByTableNumber(id);
+            return Ok(values);
+        }
+        [HttpGet("BasketListByTableNumberWithProductName")]
+        public IActionResult BasketListByTableNumberWithProductName (int id)
+        {
+            using var context = new SignalRContext();
+            var values = context.Baskets.Include(x => x.Product).Where(y => y.TableNumberId == id).Select(z => new ResultBasketListWithProducts
+            {
+                BasketId = z.BasketId,
+                Count = z.Count,
+                TableNumberId = z.TableNumberId,
+                Price = z.Price,
+                ProductId = z.ProductId,
+                ProductName = z.Product.ProductName,
+                TotalPrice = z.TotalPrice,
+            }).ToList();
             return Ok(values);
         }
     }
---
+++ SignalRWebApi/Models/ResultBasketListWithProducts.cs
@@ -0,0 +1,15 @@
+﻿using SignalR.EntityLayer.Entities;
+
+namespace SignalRWebApi.Models
+{
+    public class ResultBasketListWithProducts
+    {
+        public int BasketId { get; set; }
+        public decimal Price { get; set; }
+        public decimal Count { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int ProductId { get; set; }
+        public int TableNumberId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
