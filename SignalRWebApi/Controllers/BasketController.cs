33d48413e978e840ff4a0ccd0da63d84931eed04
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-03-18 12:19:39 +0300

    Masa numarasına göre sepet detaylarını getiren API eklendi

---
+++ SignalRWebApi/Controllers/BasketController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+
+namespace SignalRWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly  IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+        [HttpGet]
+        public IActionResult GetBasketsByTableNumber(int id) 
+        { 
+            var  values = _basketService.TGetBasketsByTableNumber(id);
+            return Ok(values);
+        }
+    }
+}
