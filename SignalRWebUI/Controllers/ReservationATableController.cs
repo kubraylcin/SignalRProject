8e4352a1bd2e59e3563e1e3a9d238328947d4632
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-03-22 06:27:42 +0300

    Rezervasyon Yapma İşlemleri ve Admin Panelinde Görüntülüyebilme

---
+++ SignalRWebUI/Controllers/ReservationATableController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.ReservationDtos;
+using System.Net.Http;
+using System.Text;
+
+namespace SignalRWebUI.Controllers
+{
+    public class ReservationATableController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ReservationATableController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateReservationDto createReservationDto)
+        {
+            
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createReservationDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7087/api/Reservation", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Default");
+            }
+            return View();
+        }
+    }
+}
