1fe3a05f18304f62c48ffa3a8f1f50388ab36fd0
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-04-25 17:12:31 +0300

    Notification CRUD işlemlerini ekledik

--- SignalRWebApi/Controllers/NotificationsController.cs
+++ SignalRWebApi/Controllers/NotificationsController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRWebApi.Controllers
 {
@@ -30,6 +33,49 @@ namespace SignalRWebApi.Controllers
 		{
 			return Ok(_notificationService.TGetAllNotificationByFalse());
 		}
+		[HttpPost]
+		public IActionResult CreateNotification(CreateNotificationDto createNotificationDto) 
+		{
+			Notification notification = new Notification()
+			{
+				Description = createNotificationDto.Description,
+				Icon = createNotificationDto.Icon,
+				Status= false,
+				Type= createNotificationDto.Type,
+				Date= Convert.ToDateTime(DateTime.Now.ToShortDateString())
+			};
+			_notificationService.TAdd(notification);
+			return Ok("Ekleeme işlemi başarı ile gerçekleşti");
+		}
 
+		[HttpDelete("{id}")]
+		public IActionResult DeleteNotification(int id)
+		{
+			var notification = _notificationService.TGetById(id);
+			_notificationService.TDelete(notification);
+			return Ok("Bildirim alanı bilgisi silindi");
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetNotification(int id)
+		{
+			var notification = _notificationService.TGetById(id);
+			return Ok(notification);
+		}
+		[HttpPut]
+		public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+		{
+			Notification notification = new Notification()
+			{
+				NotificationId = updateNotificationDto.NotificationId,
+				Description = updateNotificationDto.Description,
+				Icon = updateNotificationDto.Icon,
+				Status = updateNotificationDto.Status,
+				Type = updateNotificationDto.Type,
+				Date = updateNotificationDto.Date
+			};
+			_notificationService.TUpdate(notification);
+			return Ok("Güncelleme işlemi başarı ile gerçekleşti");
+		}
 	}
 }
