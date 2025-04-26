57c4753d9dd0b99153b56d1071b85764fd174726
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-04-26 15:34:20 +0300

    Masaların Boş veya Dolu Olarak Listelenmesi

--- SignalRWebUI/Controllers/TableNumbersController.cs
+++ SignalRWebUI/Controllers/TableNumbersController.cs
@@ -83,5 +83,18 @@ namespace SignalRWebUI.Controllers
 			}
 			return View();
 		}
+		
+		public async Task<IActionResult> TableListByStatus()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7087/api/TableNumber");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultTableNumberDto>>(jsonData);
+				return View(values);
+			}
+			return View(new List<ResultTableNumberDto>());
+		}
 	}
 }
