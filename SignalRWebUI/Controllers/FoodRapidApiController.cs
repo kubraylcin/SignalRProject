3176ca0a9b3928047ad2347c2f3a1eaffc235a80
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-05-08 18:38:51 +0300

    Rapid Api ile Dünya Mutfağı Yemek Listeleri

---
+++ SignalRWebUI/Controllers/FoodRapidApiController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.RapidApiDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class FoodRapidApiController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=60&tags=under_30_minutes"),
+                Headers =
+    {
+        { "x-rapidapi-key", "fc1d2d00eemshfa6bde7c7c12507p1e45e6jsncbc1e26e2f58" },
+        { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var jsonData = JsonConvert.DeserializeObject<TastyApiResultDto>(body);
+                var values = jsonData.results;
+                return View(values);
+            }
+           
+        }
+    }
+}
---
+++ SignalRWebUI/Dtos/RapidApiDtos/ResultTastyApi.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalRWebUI.Dtos.RapidApiDtos
+{
+    public class TastyApiResultDto
+    {
+        public List<ResultTastyApi> results { get; set; }
+    }
+    public class ResultTastyApi
+    {
+        public string Name { get; set; }
+        public string original_video_url { get; set; }
+        public int total_time_minutes { get; set; }
+        public string thumbnail_url { get; set; }
+
+    }
+}
