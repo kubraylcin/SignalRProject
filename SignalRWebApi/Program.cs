0ffd1c9d278e661e72a77d4fa7fad15553959510
Kübra Yalçın <kubrayalcin.272@gmail.com>
2025-05-10 20:09:12 +0300

    Program.cs de scopedleri extensiona taşıma işlemi

--- SignalRWebApi/Program.cs
+++ SignalRWebApi/Program.cs
@@ -1,6 +1,7 @@
 using FluentValidation;
 using SignalR.BusinessLayer.Abstract;
 using SignalR.BusinessLayer.Concrete;
+using SignalR.BusinessLayer.Container;
 using SignalR.BusinessLayer.ValidationRules.ReservationRules;
 using SignalR.DataAccessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
@@ -29,61 +30,7 @@ builder.Services.AddSignalR();
 builder.Services.AddDbContext<SignalRContext>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());// AutoMapper servislerini ekler ve geçerli derlemeyi tarayarak eþleme profillerini yükler.
 
-builder.Services.AddScoped<IAboutService,AboutManager>();
-builder.Services.AddScoped<IAboutDal,EfAboutDal>();
-
-builder.Services.AddScoped<ICategoryService, CategoryManager>();
-builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
-
-builder.Services.AddScoped<IContactService, ContactManager>();
-builder.Services.AddScoped<IContactDal, EfContactDal>();
-
-builder.Services.AddScoped<IDiscountService, DiscountManager>();
-builder.Services.AddScoped<IDiscountDal, EfDiscountDal>();
-
-// Product için baðýmlýlýk ekleme
-builder.Services.AddScoped<IProductService, ProductManager>();
-builder.Services.AddScoped<IProductDal, EfProductDal>();
-
-// Feature için baðýmlýlýk ekleme
-builder.Services.AddScoped<IFeatureService, FeatureManager>();
-builder.Services.AddScoped<IFeatureDal, EfFeatureDal>();
-
-// Reservation için baðýmlýlýk
-builder.Services.AddScoped<IReservationService, ReservationManager>();
-builder.Services.AddScoped<IReservationDal, EfReservationDal>();
-
-// SocialMedia için baðýmlýlýk
-builder.Services.AddScoped<ISocialMediaService, SocialMediaManager>();
-builder.Services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
-
-// Testimonial için baðýmlýlýk
-builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
-builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
-
-builder.Services.AddScoped<IOrderDetailService, OrderDetailManager>();
-builder.Services.AddScoped<IOrderDetailDal, EfOrderDetailDal>();
-
-builder.Services.AddScoped<IOrderService, OrderManager>();
-builder.Services.AddScoped<IOrderDal, EfOrderDal>();
-
-builder.Services.AddScoped<IMoneyCaseService, MoneyCaseManager>();
-builder.Services.AddScoped<IMoneyCaseDal, EfMoneyCaseManager>();
-
-builder.Services.AddScoped<ITableNumberService, TableNumberManager>();
-builder.Services.AddScoped<ITableNumberDal, EfTableNumberDal>();
-
-builder.Services.AddScoped<ISliderService, SliderManager>();
-builder.Services.AddScoped<ISliderDal, EfSliderDal>();
-
-builder.Services.AddScoped<IBasketService, BasketManager>();
-builder.Services.AddScoped<IBasketDal, EfBasketDal>();
-
-builder.Services.AddScoped<INotificationService, NotificationManager>();
-builder.Services.AddScoped<INotificationDal, EfNotificationDal>();
-
-builder.Services.AddScoped<IMessageService, MessageManager>();
-builder.Services.AddScoped<IMessageDal, EfMessageDal>();
+builder.Services.ContainerDependencies();
 
 builder.Services.AddValidatorsFromAssemblyContaining<CreateReservationValidation>();
 
